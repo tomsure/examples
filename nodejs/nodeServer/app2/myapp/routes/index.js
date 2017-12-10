b4d2047118f78a2720ba6793786ec0e29a24a7da
tomsure <491583156@qq.com>
2017-12-10 20:22:45 +0800

    新组建仓库

---
+++ nodejs/nodeServer/app2/myapp/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '罗维佳' });
+});
+
+module.exports = router;
