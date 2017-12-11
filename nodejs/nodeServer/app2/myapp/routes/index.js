19db1bf512dbbc71af74d94c5a77915bb2bf7319
tomsure <491583156@qq.com>
2017-12-11 21:50:04 +0800

    11

--- nodejs/nodeServer/app2/myapp/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '罗维佳' });
-});
-
-module.exports = router;
---
+++ nodejs/nodeServer/mockjs/3.express send()/myapp/routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '1111' });
+});
+
+router.get('/mockjs', function(req, res, next) {
+	var Mock = require('mockjs');
+	var data = Mock.mock({
+	    'list|1-10': [{
+	        'id|+1': 1
+	    }]
+	}); 
+    var ret= JSON.stringify(data, null, 4)
+    console.log(ret)
+    res.render('index', { title: ret });
+});
+
+router.get('/mockapi', function(req, res, next) {
+	var Mock = require('mockjs');
+	var data = Mock.mock({
+	    'list|1-10': [{
+	        'id|+1': 1
+	    }]
+	}); 
+    var ret= JSON.stringify(data, null, 4)
+    console.log(ret)
+    res.send(ret);
+});
+module.exports = router;
