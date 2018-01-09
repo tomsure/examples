45222a58ff15d3168d3c944fe1793356653a77fe
tomsure <491583156@qq.com>
2018-01-09 23:42:04 +0800

    vue-cli

--- nodejs/nodeServer/vue2.0/教程/1、基础/1、安装/3、命令行工具 (CLI又叫脚手架)/2、使用方式/1、使用官方模板/1、使用webpack模板/example/vue-Project1/.eslintrc.js
+++ nodejs/nodeServer/vue2.0/教程/1、基础/1、安装/3、命令行工具 (CLI又叫脚手架)/2、使用方式/1、使用官方模板/1、使用webpack模板/example/vue-Project1/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     browser: true,
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: 'standard',  //标准规则，具体可以参考上面的地址
   // required to lint *.vue files
   plugins: [
     'html'
@@ -18,8 +18,8 @@ module.exports = {
   // add your custom rules here
   rules: {
     // allow async-await
-    'generator-star-spacing': 'off',
+    'generator-star-spacing': 'off',  //忽略规则检查
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'  //不允许debugger调试的条件，如果是开发环境则可以使用degug，如果生产环境，则不允许
   }
 }
