732fa2a429bbd8edf949cb31b1835a67cd500de9
tomsure <491583156@qq.com>
2018-12-17 23:30:04 +0800

    new

--- nodejs/nodeServer/vue2.0/1,API/api/src/router/index.js
+++ nodejs/nodeServer/vue2.0/1,API/api/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/5、选项-生命周期钩子/10、整体demo'
-import watchDataChange from '@/components/3、选项-数据/6、watch（观察者、监听）/2、监听数据变化/watchDataChange'
+import HelloWorld from '@/components/3、选项/3、生命周期钩子/10、整体demo'
+import watchDataChange from '@/components/3、选项/1、数据/6、watch（观察者、监听）/2、监听数据变化/watchDataChange'
 Vue.use(Router)
 
 export default new Router({
